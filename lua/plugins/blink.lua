21e4a6c24e35ca77a78287826f58e2bb6e9ae3dd
brownhounds <brownhounds739@gmail.com>
2025-02-17 22:06:13 +0000

    Add lsp keybinds

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,15 +1,15 @@
 return {
   {
-    'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
+    "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
 
-    version = '*',
+    version = "*",
     opts = {
       -- 'default' for mappings similar to built-in completion
       -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- See the full "keymap" documentation for information on defining your own keymap.
-      keymap = { preset = 'default' },
+      keymap = { preset = "super-tab" },
 
       appearance = {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -18,23 +18,21 @@ return {
         use_nvim_cmp_as_default = true,
         -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'normal',
+        nerd_font_variant = "normal",
       },
 
-      signature = { enabled = true, window = { border = 'single' } },
+      signature = { enabled = true, window = { border = "single" } },
 
       completion = {
-        documentation = { window = { border = 'single' } },
+        documentation = { window = { border = "single" } },
         ghost_text = { enabled = false },
       },
 
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { "lsp", "path", "snippets", "buffer" },
       },
-
     },
-  }
+  },
 }
-
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,25 +1,31 @@
 return {
   {
-    'williamboman/mason.nvim'
+    "williamboman/mason.nvim",
   },
   {
-    'williamboman/mason-lspconfig.nvim'
+    "williamboman/mason-lspconfig.nvim",
   },
   {
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     dependencies = {
       {
-        'folke/lazydev.nvim',
-        ft = 'lua', -- only load on lua files
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
         opts = {
           library = {
             -- See the configuration section for more details
             -- Load luvit types when the `vim.uv` word is found
-            { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
           },
         },
       },
     },
-  }
+    config = function()
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+    end,
+  },
 }
-
